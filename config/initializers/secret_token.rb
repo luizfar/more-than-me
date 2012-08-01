af11883ffd551916ed3135b576093368d16f4faf
Luiz Fernando Ribeiro <luizfar@gmail.com>
2012-08-01 19:58:18 -0400

    Luiz/Dahlia - First commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MoreThanMe::Application.config.secret_token = '3c37ab073755d7dd375b2ced25294b3a21032c000955204a2b917205f1676ecedba844a0528a5a545e41bc3546c34179477fc9ffc39de4072903dd33c327269b'
