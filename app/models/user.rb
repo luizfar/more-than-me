05bdfd533e58ed40ecae70cbf9a9f6f14b2c3a01
Luiz Ribeiro <luizfar@gmail.com>
2012-12-16 14:53:05 -0200

    Add first and last name, location and amount raised to User.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,15 +4,26 @@ class User < ActiveRecord::Base
   # :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :timeoutable
-
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
-
+  attr_accessible :email,
+    :password,
+    :password_confirmation,
+    :first_name,
+    :last_name,
+    :location
+    :remember_me
   after_create :send_welcome_email
+
+  validates_presence_of :first_name, :last_name, :location
+  before_create :initialize_amount_raised
 
   private
 
   def send_welcome_email
     UserMailer.welcome_email(self).deliver
+  end
+
+  def initialize_amount_raised
+    self.amount_raised_in_cents ||= 0
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120829235353) do
+ActiveRecord::Schema.define(:version => 20121216164110) do
 
   create_table "campaigns", :force => true do |t|
     t.string   "title"
@@ -33,6 +33,10 @@ ActiveRecord::Schema.define(:version => 20120829235353) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "location"
+    t.integer  "amount_raised_in_cents"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
