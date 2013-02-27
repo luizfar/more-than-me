f62f4248ec485ff36fa86c211486f73ada10ef3f
Damon Maneice <Thoughtworker@DM8998.local>
2013-02-27 18:48:26 -0300

    added displaying girls in school

--- app/models/campaign.rb
+++ app/models/campaign.rb
@@ -23,9 +23,7 @@ class Campaign < ActiveRecord::Base
   def days_left
     (end_date.to_date - Date.today).to_i
   end
-
-  private
-
+  
   def number_of_girls_in_school  
     (amount_raised_in_cents / @amount_per_year_in_cents).to_i
   end
